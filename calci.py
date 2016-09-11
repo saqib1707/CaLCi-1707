bf9301b3e35ff70ee543dddb7a77e0fd15159288
saqib1707 <azimsaqib96@gmail.com>
2016-09-11 12:33:36 +0300

    midsems going...basic interface and functioning of buttons achieved

--- calci.py
+++ calci.py
@@ -52,18 +52,32 @@ def demo_QButton():
 if __name__=='__main__':
     demo_QButton()
 """
-class make_QButton(QWidget):
-    def __init__(self,btn_no):
+class make_Button(QWidget):
+    def __init__(self,btn_repr):
         QWidget.__init__(self,None)
-        self.btn=QPushButton(str(btn_no),self)
-        self.btn.clicked.connect(self.make_CallBack(btn_no))
+        self.btn=QPushButton(btn_repr,self)
+        self.btn.clicked.connect(self.make_CallBack(btn_repr))
 
-    def make_CallBack(self,btn_no):
+    def make_CallBack(self,btn_repr):
         def CallBack():
-            self.btn.setText('I am '+str(btn_no))
+            lineEdit.line_edit.setText(btn_repr)
+            self.btn.setText('I am '+btn_repr)
         return CallBack
+"""
+class Operand(QWidget):
+    def __init__(self):
+        self.operand_button=QPushButton(self,)
+        self.operand_button.clicked.connect(self.make_callback())
+
+    def make_callback(self):
+        def callback():
+            lineEdit.setText()
 
 
+        self.minus_button=QPushButton(self,'-')
+        self.into_button=QPushButton(self,'x')
+        self.divide_button=QPushButton(self,'/')
+"""
 class make_LineEdit(QWidget):
     def __init__(self):
 
@@ -76,26 +90,40 @@ def window():
     app=QApplication(sys.argv)
     window=QWidget()
 
+    global lineEdit
     lineEdit=make_LineEdit()
-    vBox=QVBoxLayout()
+    vBox_outer=QVBoxLayout()
     grid=QGridLayout()
-    hBox1=QHBoxLayout()
 
+    hBox_number_button=QHBoxLayout()
+    vBox_operand_button=QVBoxLayout()
+
+    hBox_button=QHBoxLayout()
+    button_list=['+','-','x','/']
     count=0
     for i in range(0,3):
         for j in range(0,3):
             count+=1
-            number_btn=make_QButton(count)
+            number_btn=make_Button(str(count))
             grid.addWidget(number_btn,i,j)
-    hBox1.addLayout(grid)
+    hBox_number_button.addLayout(grid)
 
-    hBox2=QHBoxLayout()
-    hBox2.addWidget(lineEdit.line_edit)
+    for i in range(len(button_list)):
+        operand_btn=make_Button(button_list[i])
+        vBox_operand_button.addWidget(operand_btn)
 
-    vBox.addLayout(hBox2)
-    vBox.addLayout(hBox1)
 
-    window.setLayout(vBox)
+    hBox_button.addLayout(hBox_number_button)
+    hBox_button.addLayout(vBox_operand_button)
+
+    hBox_lineEdit=QHBoxLayout()
+    hBox_lineEdit.addWidget(lineEdit.line_edit)
+
+    vBox_outer.addLayout(hBox_lineEdit)
+    vBox_outer.addLayout(hBox_button)
+
+    window.setFixedSize(450,500)
+    window.setLayout(vBox_outer)
     window.setWindowTitle('CaLCi-1707')
     window.show()
     app.exec_()
