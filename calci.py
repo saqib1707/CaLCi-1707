3ea5483edaa665e0168d06b25c9b746cf8721979
saqib1707 <azimsaqib96@gmail.com>
2016-09-14 14:53:48 +0300

    first layout ready

--- calci.py
+++ calci.py
@@ -1,57 +1,9 @@
 import sys
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
-"""
-class QButton(QWidget):
-    def __init__(self,button_no):
-        QtGui.QWidget.__init__(self, None)
-        self.button = QtGui.QPushButton('Button'+str(button_no), self)
-        self.name=str(button_no)
 
-        self.button.clicked.connect(self.make_calluser(self.name,button_no))
-    def make_calluser(self, name,no):
-        def calluser():
-            self.button.setText('btn-'+str(no))
-            text=lineEdit.e1.text()
-            print int(text)
-        return calluser
+import logicOfCalci
 
-class LineEdit(QtGui.QWidget):
-    def __init__(self):
-        QtGui.QWidget.__init__(self, None)
-        self.e1=QtGui.QLineEdit()
-        self.e1.setMaxLength(20)
-        self.e1.setAlignment(QtCore.Qt.AlignRight)
-        self.e1.setFont(QtGui.QFont('Arial',20))
-  
-def demo_QButton():
-
-    app = QtGui.QApplication(sys.argv)
-    window=QtGui.QWidget()
-    grid=QtGui.QGridLayout()
-    
-    global lineEdit
-    lineEdit=LineEdit()
-
-    grid.addWidget(lineEdit.e1,0,0)
-
-    for i in range(0,4):
-        tb = QButton(i)
-        grid.addWidget(tb,i+4,0)
-    
-
-    
-    window.setLayout(grid)
-    window.setGeometry(200,200,0,0)
-    window.setFixedSize(400,400)
-    window.setWindowTitle('CaLCi-1707')
-    window.show()
-    
-    sys.exit(app.exec_())
-
-if __name__=='__main__':
-    demo_QButton()
-"""
 class make_Button(QWidget):
     def __init__(self,btn_repr):
         QWidget.__init__(self,None)
@@ -60,29 +12,24 @@ class make_Button(QWidget):
 
     def make_CallBack(self,btn_repr):
         def CallBack():
-            lineEdit.line_edit.setText(btn_repr)
-            self.btn.setText('I am '+btn_repr)
-        return CallBack
-"""
-class Operand(QWidget):
-    def __init__(self):
-        self.operand_button=QPushButton(self,)
-        self.operand_button.clicked.connect(self.make_callback())
+            lineEdit.line_edit.insert(btn_repr)
+            #self.btn.setText('I am '+btn_repr)
+            
+        def result():
+            global text
+            text=lineEdit.line_edit.text()
+            logicOfCalci.main(text)
+        
+        if btn_repr!='=':
+            return CallBack
+        else:
+            return result
 
-    def make_callback(self):
-        def callback():
-            lineEdit.setText()
-
-
-        self.minus_button=QPushButton(self,'-')
-        self.into_button=QPushButton(self,'x')
-        self.divide_button=QPushButton(self,'/')
-"""
 class make_LineEdit(QWidget):
     def __init__(self):
 
         self.line_edit=QLineEdit()
-        self.line_edit.setMaxLength(20)
+        self.line_edit.setMaxLength(40)
         self.line_edit.setAlignment(Qt.AlignRight)
         self.line_edit.setFont(QFont('Arial',15))
 
@@ -94,35 +41,39 @@ def window():
     lineEdit=make_LineEdit()
     vBox_outer=QVBoxLayout()
     grid=QGridLayout()
+    answer_label=QLabel()
+    answer_label.setAlignment(Qt.AlignRight)
+    hBox_answerLabel=QHBoxLayout()
+    hBox_answerLabel.addWidget(answer_label)
 
-    hBox_number_button=QHBoxLayout()
-    vBox_operand_button=QVBoxLayout()
-
-    hBox_button=QHBoxLayout()
-    button_list=['+','-','x','/']
+    button_list=[['+','<x','C'],['-','sqrt','pow'],['x','(',')'],['/','=','y']]
+    another_list=[['0','.','%']]
     count=0
-    for i in range(0,3):
+    for i in range(0,4):
         for j in range(0,3):
-            count+=1
-            number_btn=make_Button(str(count))
-            grid.addWidget(number_btn,i,j)
-    hBox_number_button.addLayout(grid)
+            if i!=3:
+                count+=1
+                number_btn=make_Button(str(count))
+                grid.addWidget(number_btn,i,j)
+            else:
+                another_button=make_Button(another_list[0][j])
+                grid.addWidget(another_button,i,j)
 
-    for i in range(len(button_list)):
-        operand_btn=make_Button(button_list[i])
-        vBox_operand_button.addWidget(operand_btn)
-
-
-    hBox_button.addLayout(hBox_number_button)
-    hBox_button.addLayout(vBox_operand_button)
+    for i in range(0,4):
+        for j in range(3,6):
+            operand_button=make_Button(button_list[i][j-3])
+            grid.addWidget(operand_button,i,j)
+    changeSlide_btn=make_Button('\/')
+    grid.addWidget(changeSlide_btn,4,2)
 
     hBox_lineEdit=QHBoxLayout()
     hBox_lineEdit.addWidget(lineEdit.line_edit)
 
+    vBox_outer.addLayout(hBox_answerLabel)
     vBox_outer.addLayout(hBox_lineEdit)
-    vBox_outer.addLayout(hBox_button)
+    vBox_outer.addLayout(grid)
 
-    window.setFixedSize(450,500)
+    window.setGeometry(20,40,580,400)
     window.setLayout(vBox_outer)
     window.setWindowTitle('CaLCi-1707')
     window.show()
