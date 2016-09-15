2f12735d34d393876d92654d4ed41d9215eab621
saqib1707 <azimsaqib96@gmail.com>
2016-09-15 10:41:56 +0300

    one side completed

--- calci.py
+++ calci.py
@@ -3,6 +3,19 @@ from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
 import logicOfCalci
+import math
+
+
+#some constants
+
+e=2.7
+pi=math.pi
+R=8.314
+h=6.626e-34
+k=1.38e-23
+z=1
+t=2
+
 
 class make_Button(QWidget):
     def __init__(self,btn_repr):
@@ -13,17 +26,23 @@ class make_Button(QWidget):
     def make_CallBack(self,btn_repr):
         def CallBack():
             lineEdit.line_edit.insert(btn_repr)
-            #self.btn.setText('I am '+btn_repr)
             
         def result():
-            global text
             text=lineEdit.line_edit.text()
-            logicOfCalci.main(text)
+            final_result=logicOfCalci.main(text)
+            answer_label.setText(str(final_result[0]))
         
-        if btn_repr!='=':
-            return CallBack
-        else:
+        def clear():
+            lineEdit.line_edit.clear()
+        if btn_repr=='=':
             return result
+        elif btn_repr=='C':
+            return clear
+        #elif btn_repr=='\/':
+            #self.changeSlide()
+        else:
+            return CallBack
+    #def changeSlide(self):
 
 class make_LineEdit(QWidget):
     def __init__(self):
@@ -41,6 +60,8 @@ def window():
     lineEdit=make_LineEdit()
     vBox_outer=QVBoxLayout()
     grid=QGridLayout()
+    grid_constants=QGridLayout()
+    global answer_label
     answer_label=QLabel()
     answer_label.setAlignment(Qt.AlignRight)
     hBox_answerLabel=QHBoxLayout()
@@ -48,6 +69,7 @@ def window():
 
     button_list=[['+','<x','C'],['-','sqrt','pow'],['x','(',')'],['/','=','y']]
     another_list=[['0','.','%']]
+    constants_list=['e','pi','R','h','k','t','z']
     count=0
     for i in range(0,4):
         for j in range(0,3):
@@ -66,14 +88,19 @@ def window():
     changeSlide_btn=make_Button('\/')
     grid.addWidget(changeSlide_btn,4,2)
 
+    for i in range(len(constants_list)):
+        constants_btn=make_Button(constants_list[i])
+        grid_constants.addWidget(constants_btn,0,i)
+    
     hBox_lineEdit=QHBoxLayout()
     hBox_lineEdit.addWidget(lineEdit.line_edit)
 
     vBox_outer.addLayout(hBox_answerLabel)
     vBox_outer.addLayout(hBox_lineEdit)
+    vBox_outer.addLayout(grid_constants)
     vBox_outer.addLayout(grid)
 
-    window.setGeometry(20,40,580,400)
+    window.setGeometry(20,40,580,550)
     window.setLayout(vBox_outer)
     window.setWindowTitle('CaLCi-1707')
     window.show()
